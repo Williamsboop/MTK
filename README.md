325f16cd389b966e3820492aff5bdeb498ad895e
Williamsboop <williamsacameron2001@gmail.com>
2025-07-11 21:36:04 -0500

    Added Logo. Added CLAMP class.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+![alt text](MTK_LOGO.png)
+
 # MTK
  Various prewritten functions and classes to aid in programming.
---
+++ clamp.py
@@ -0,0 +1,33 @@
+from typing import Any, Self
+
+class CLAMP:
+    @staticmethod
+    def err(err: str) -> str:
+        return f"\n !!! CLAMP Error: {err} !!! \n"
+    
+    def __new__(cls: Self, _min_: Any, _mid_: Any, _max_: Any) -> Any:
+        
+        # Guard clause for banned types...
+        for val in [_min_, _mid_, _max_]:
+            if isinstance(val, (str, list, dict, set, tuple, complex)):
+                return cls.err(str(f"Cannot use type: {type(val).__name__} for value: {val}."))
+         
+        # Gaurd clause to determine if min > max...
+        if (_min_ > _max_):
+            return cls.err(str(f"Minimum value cannot be greater than maximum value."))
+            
+        # Gaurd clause to determine if min > mid...
+        if (_min_ > _mid_):
+            return cls.err(str(f"Minimum value cannot be greater than middle value."))
+            
+         # Gaurd clause to determine if mid > max...
+        if (_mid_ > _max_):
+            return cls.err(str(f"Middle value cannot be greater than maximum value."))
+        
+        # Returns either a clamped integer or float depending on values...
+        if all(isinstance(val, int) for val in [_min_, _mid_, _max_]):
+            return int(max(min(_mid_, _max_), _min_))
+        else:
+            if (max(min(_mid_, _max_), _min_).is_integer()):
+                return int(max(min(_mid_, _max_), _min_))
+            return float(max(min(_mid_, _max_), _min_))
